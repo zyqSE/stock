d1f76f7eb4cc318980b0a5b8bbeaba7a7c8804f6
stock <stock@github.com>
2018-12-16 22:57:28 +0800

    修改代码位数不足6时无法查询的问题

--- app/controllers/temp/history.py
+++ app/controllers/temp/history.py
@@ -12,9 +12,11 @@ def history (code, start, end):
 
 if __name__ == '__main__':
   code_file=open("app/controllers/temp/code.txt","r")
+  #code_file=open("code.txt","r")
   code = code_file.read().splitlines()
   code_file.close()
   code=''.join(code)
+  code=code.zfill(6)
   start = datetime.now()-timedelta(days = 180)
   start = start.strftime('%Y-%m-%d')
   end = datetime.now().strftime('%Y-%m-%d')
