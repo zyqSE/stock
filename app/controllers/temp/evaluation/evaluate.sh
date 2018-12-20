55c6bc4f7b488b2182971b1e6077e95066a243f1
stock <stock@github.com>
2018-12-20 11:54:16 +0800

    删除脚本调试信息

--- app/controllers/temp/evaluation/evaluate.sh
+++ app/controllers/temp/evaluation/evaluate.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-path=pwd
-echo "wwwwwwwwwwwwwwwwwwww"
-
-echo path
 python app/controllers/temp/evaluation/simple.py
 python app/controllers/temp/evaluation/old_simple.py
 python app/controllers/temp/evaluation/csv2json.py
